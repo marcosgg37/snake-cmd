8a8f0790892a70c051dd08bee0d935c8e232a402
marcosgg37 <marcosgg37@educastur.es>
2025-12-17 21:06:48 +0100

    Implementada la interfaz en las clases

--- Snake/src/main/scenes/GameScene.java
+++ Snake/src/main/scenes/GameScene.java
@@ -1,5 +1,25 @@
 package main.scenes;
 
-public class GameScene {
+public class GameScene implements IScene {
+
+	
+	
+	@Override
+	public void loadScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unloadScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- Snake/src/main/scenes/MenuScene.java
+++ Snake/src/main/scenes/MenuScene.java
@@ -1,5 +1,23 @@
 package main.scenes;
 
-public class MenuScene {
+public class MenuScene implements IScene {
+
+	@Override
+	public void loadScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unloadScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
