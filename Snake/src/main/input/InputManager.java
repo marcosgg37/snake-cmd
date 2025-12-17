cc96fe2f9b5478ec2ad91f9af186a70c9f6c1ba7
marcosgg37 <marcosgg37@educastur.es>
2025-12-17 17:14:09 +0100

    Quitados métodos obsoletos (comentados)

--- Snake/src/main/input/InputManager.java
+++ Snake/src/main/input/InputManager.java
@@ -7,33 +7,11 @@ public class InputManager {
 	public static final char LEFT_DIRECTION = 'a', RIGHT_DIRECTION = 'd', UP_DIRECTION = 'w', DOWN_DIRECTION = 's';
 	private static final char DEFAULT_STARTING_DIRECTION = RIGHT_DIRECTION;
 	
-	/*
-	private Runnable runInputManager = new Runnable() {
-		public void run() {
-			do {
-				waitForDirection();
-			} while(true);
-		}
-	};
-	*/
-	
-	//private Scanner in;
 	private char direction;
 		
 	public InputManager() {
-		//this.in = new Scanner(System.in);
 		this.direction = DEFAULT_STARTING_DIRECTION;
 	}
-	
-	/*
-	public void waitForDirection() {
-		String input = in.nextLine();
-		if (input.isBlank())
-			input = String.valueOf(direction);
-		this.direction = input.charAt(0);
-		
-	}
-	*/
 	
 	public void setDirection(char dir) {
 		if (!"wasd".contains(dir + "")) return;
@@ -44,10 +22,4 @@ public class InputManager {
 	public char getDirection() {
 		return this.direction;
 	}
-	
-	/*
-	public Runnable getRunnable() {
-		return this.runInputManager;
-	}
-	*/
 }
